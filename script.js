12adc35bed8f223043f76e848fdf95349b1942f7
Michelle Lee <michelle.lee@wattpad.com>
2016-07-16 12:33:48 -0400

    First map yay

--- script.js
+++ script.js
@@ -1,34 +1,103 @@
-var map;
+ var map;
 
-window.onload = function() {
-  map = new GMaps( {
-    div: '#map',
-    lat: 43.6569619,
-    lng: -79.3858808
-  } );
+ function loadResults( data ) {
+   var items, markers_data = [];
+   if ( data.length > 0 ) {
+     items = data;
 
-  GMaps.geolocate( {
-    success: function( position ) {
-      map.setCenter( position.coords.latitude, position.coords.longitude );
-      console.log("position.coords = ", position.coords);
-      map.addMarker({
-        lat: position.coords.latitude, lng: position.coords.longitude,
-        title: 'Your location',
-        infoWindow: {
-          content: '<p>You are here</p>'
-        }
-      });
-    },
-    error: function( error ) {
-      alert( 'Geolocation failed: ' + error.message );
-    },
-    not_supported: function() {
-      alert( "Your browser does not support geolocation" );
-    },
-    always: function() {
-      alert( "Done!" );
-    }
-  } );
+     for ( var i = 0; i < items.length; i++ ) {
+       var item = items[ i ];
 
+       if ( item.Latitude != undefined && item.Longitude != undefined ) {
 
-}
+         markers_data.push( {
+           lat: item.Latitude,
+           lng: item.Longitude,
+           title: item.name,
+           infoWindow: {
+             content: '<p>' + item.Org_Name + '</p>'
+           }
+         } );
+       }
+     }
+   }
+
+   map.addMarkers( markers_data );
+ }
+
+ function printResults( data ) {
+   $( '#foursquare-results' ).text( JSON.stringify( data ) );
+ }
+
+ $( document ).on( 'click', '.pan-to-marker', function( e ) {
+   e.preventDefault();
+
+   var position, lat, lng, $index;
+
+   $index = $( this ).data( 'marker-index' );
+
+   position = map.markers[ $index ].getPosition();
+
+   lat = position.lat();
+   lng = position.lng();
+
+   map.setCenter( lat, lng );
+ } );
+
+ $( document ).ready( function() {
+   map = new GMaps( {
+     div: '#map',
+     lat: 43.6569619,
+     lng: -79.3858808
+   } );
+
+   map.on( 'marker_added', function( marker ) {
+     var index = map.markers.indexOf( marker );
+     $( '#results' ).append( '<li><a href="#" class="pan-to-marker" data-marker-index="' + index + '">' + marker.title + '</a></li>' );
+
+     if ( index == map.markers.length - 1 ) {
+       map.fitZoom();
+     }
+   } );
+
+   var xhr = $.getJSON( 'https://raw.githubusercontent.com/PoliHackSteppingStones/SteppingStones/master/yow.json' );
+
+   xhr.done( printResults );
+   xhr.done( loadResults );
+ } );
+
+ // var map;
+
+ // var data = https://raw.githubusercontent.com/PoliHackSteppingStones/SteppingStones/master/yow.json
+
+ // window.onload = function() {
+ //   map = new GMaps( {
+ //     div: '#map',
+ //     lat: 43.6569619,
+ //     lng: -79.3858808
+ //   } );
+
+ //   GMaps.geolocate( {
+ //     success: function( position ) {
+ //       map.setCenter( position.coords.latitude, position.coords.longitude );
+ //       console.log("position.coords = ", position.coords);
+ //       map.addMarker({
+ //         lat: position.coords.latitude, lng: position.coords.longitude,
+ //         title: 'Your location',
+ //         infoWindow: {
+ //           content: '<p>You are here</p>'
+ //         }
+ //       });
+ //     },
+ //     error: function( error ) {
+ //       alert( 'Geolocation failed: ' + error.message );
+ //     },
+ //     not_supported: function() {
+ //       alert( "Your browser does not support geolocation" );
+ //     },
+ //     always: function() {
+ //       alert( "Done!" );
+ //     }
+ //   } );
+
+ // }
